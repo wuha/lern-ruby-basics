b2ddeacf388d0468a1b9a9ca72c6906d7a356ad9
Simon Goetz <simon.goetz@me.com>
2013-09-12 16:45:21 +0200

    create some test classes

--- hello-world.rb
+++ hello-world.rb
@@ -1,1 +1,41 @@
-puts 'Hello World'
+
+class TestClass
+
+	def initialize (farbe='gruen')
+		puts farbe
+	end
+
+
+	def methode_ohne_paramter
+		puts 'Test'
+		puts 'Test'
+		puts 'test'	
+		methode_mit_default_paramter
+	end
+
+	def methode_mit_paramter(text)
+		puts text
+		puts text
+		puts text
+	end
+
+	private
+
+	def methode_mit_default_paramter(text='Hallo Ihr')
+		puts text
+		puts text
+		puts text
+	end
+
+end
+
+class TestClass2 < TestClass
+
+
+
+end
+
+
+meinTest=TestClass2.new
+
+meinTest.methode_ohne_paramter
